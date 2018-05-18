cd5808f9d4f75b2c03399bb970a2949cb6784396
leon <leon_a_d@yahoo.com>
2018-05-18 12:12:01 +0100

    wip

---
+++ bin/command_line_output_functions.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+function run() {
+    eval "$@ > /dev/null"
+    echo $?
+}
+
+function red(){
+    local red='\033[0;31m'
+    local no_colour='\033[0m'
+    local text=$1
+    echo "${red}${text}${no_colour}"
+}
+
+function green(){
+    local red='\033[0;32m'
+    local no_colour='\033[0m'
+    local text=$1
+    echo "${red}${text}${no_colour}"
+}
+
+function error() {
+    local text=$1
+    echo $(red "[ ERROR ] ${text}")
+}
+
+function ok(){
+    local text=$1
+    echo $(green "[ OK ] ${text}")
+}
+
+function say(){
+    echo -e "$@"
+}
---
+++ exercises/ansible/first_playbook/readme.md
@@ -0,0 +1,9 @@
+# Ansible
+## Introduction
+This is exercise is not supposed to be an exhaustive introduction to ansible. For more on this look [here]
+
+## Objectives
+Write a playbook
+
+## Exercise
+Install apache on to a server.
